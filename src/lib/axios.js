02eb442f69b9010c09949b92cb9cadab0eca3235
ayushM <ayushmehrotra099@gmail.com>
2025-03-15 22:25:17 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://backend-51iyr7rji-ayushoopps-projects.vercel.app/api/",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://backend-sooty-eight.vercel.app/api/",
   withCredentials: true,
 });
